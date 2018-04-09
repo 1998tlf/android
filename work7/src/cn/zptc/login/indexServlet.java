41b5389a5df88f1481921fb556fe2bc7cfe9dfa0
1998tlf <1164597883@qq.com>
2018-04-09 11:13:16 +0800

    javaweb

---
+++ work7/src/cn/zptc/login/indexServlet.java
@@ -0,0 +1,60 @@
+package cn.zptc.login;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class indexServlet
+ */
+@WebServlet("/indexServlet")
+public class indexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private HttpSession session;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public indexServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=utf-8");
+		HttpSession session=request.getSession();
+		User user = (User) session.getAttribute("user");
+		if(user==null) {
+			response.getWriter().print("Äú»¹Ã»µÇÂ¼£¬Çë<a href='/work7/login.html'>µÇÂ¼</a>");
+		}else {
+			response.getWriter().print("ÄúÒÑµÇÂ¼"+user.getUsername()+"!");
+			response.getWriter().print("<a href='/work7/LogoutServlet'>ÍË³ö</a>");
+			Cookie cookie=new Cookie("JSESSIONID",session.getId());
+			cookie.setMaxAge(60*30);
+			cookie.setPath("/login");
+			response.addCookie(cookie);
+			
+		}
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
